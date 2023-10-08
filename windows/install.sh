b8327db44eac17f79d1d252b3d1c7a3c3e0df21f
Pinardy <pinardy.yang@gmail.com>
2023-10-08 20:23:55 +0800

    add vscode keybindings for windows

--- windows/install.sh
+++ windows/install.sh
@@ -1,6 +1,9 @@
 dir=~/dotfiles
 files=".aliases .bash_profile .bashrc .bash_prompt .vimrc .inputrc"
 
+echo "=== SYMLINKS ==="
+echo
+
 for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -sf "$dir/$file" ~/$file
@@ -13,4 +16,21 @@ if [ -d "$terminalDir" ]; then
     cp ./windows-terminal-settings.json "$terminalDir/LocalState/settings.json"
     cp ./git-bash.ico "$terminalDir/RoamingState"
     cp ./ubuntu.ico "$terminalDir/RoamingState"
+fi
+
+# VSCode settings
+vscodeDir=~/AppData/Roaming/Code/User
+keybindingsFile=keybindings.json
+snippetsFile=snippets.json
+
+if [ -d "$vscodeDir" ]; then
+    echo
+    echo "=== VSCode ==="
+    echo
+
+    cp "$dir/$keybindingsFile" "$vscodeDir/$keybindingsFile"
+    echo "Copying $keybindingsFile to VSCode settings directory."
+
+    cp "$dir/$snippetsFile" "$vscodeDir/snippets/snippets.code-snippets"
+    echo "Copying $snippetsFile to VSCode settings directory."
 fi
